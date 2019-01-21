1ba26dc247f7c6588344f61eeae825c8c6f5546c
mrech <rivato.morena@gmail.com>
2019-01-21 19:26:45 +0100

    ex45.py - Lost in the forest

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 - SOURCE: Learn Python the Hard Way. Third Edition
 - CODE: Python 3.6
 - OS: Ubuntu 18.04 LTS
+
+
+ex45.py - YOU MAKE A GAME: Lost in the forest
+- dependency: engine.py
+- run: python3 ex45.py main_path
--- ex45.py
+++ ex45.py
@@ -8,7 +8,7 @@
 - play
 * Scene
     - enter
-    * Drawn
+    * Drown
     * Shot
     * Accident
     * Main Path
@@ -17,46 +17,64 @@
     * Escape Van
 """
 
+# Basic import for the game
 from sys import exit
-from random import randint
+import random as rand
 from sys import argv
 from engine import *
 script, room = argv
+
+# Base class scene
 
 
 class Scene(object):
 
     def enter(self):
-        print("This scene is not yet configured. Subclass it and implement enter().")
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
+        print("\n--------------")
         exit(1)
 
+# Death scene - Drown
 
-class Drawn(Scene):
+
+class Drown(Scene):
 
     def enter(self):
-        print("The current of the river is so strong that you hit a rock,")
-        print("you lose consciousness and drawn on the deep water.")
+        print("The current of the river is too strong. You hit a rock,")
+        print("you lose consciousness and drown in the deep water.")
+        print("\n--------------")
         exit(1)
+
+# Death scene - shot
 
 
 class Shot(Scene):
 
     def enter(self):
-        print("The hunter is a very jealous guy, he strongly believe that you want")
-        print("all the animals of the forest and let him starve. You left him with")
-        print("not choice. He shot you straight in the head.")
+        print("The hunter is a very jealous guy,")
+        print("he strongly believe that you want")
+        print("all the animals of the forest and let him starve.")
+        print("You left him with")
+        print("not other choice. He shoots you straight in the head.")
+        print("\n--------------")
         exit(1)
+
+# Death scene - accident
 
 
 class Accident(Scene):
 
     def enter(self):
-        print("The driver bored to death waiting for you got heavely drunk,")
-        print("so your escape from the forest is not an easy one. Unfortunatly,")
-        print("you can not help since you'are without driver lincense.")
-        print("You invede the opposite lane when a track was coming.")
-        print("The car accident is fatal.")
+        print("The driver bored to death waiting for you got heavely drunk.")
+        print("Unfortunatly,")
+        print("you can not help, since you do not have a driver lincense.")
+        print("The van invaded the opposite lane when a track was coming.")
+        print("The accident is fatal.")
+        print("--------------")
         exit(1)
+
+# MainPath is the start of the game
 
 
 class MainPath(Scene):
@@ -69,53 +87,142 @@ class MainPath(Scene):
         print("Maybe somebody is in there..")
 
         action = input(
-            "> 1: Enter to ask info \n > 2: Continue to run down the path")
+            """\n > 1: Enter to ask info
+            \n > 2: Continue to run down the path
+            \n > """)
 
-        if action == 1:
+        if action == "1":
+            print("\n--------------")
             print("You decide to enter into the shelter.")
             print("Dead animals hang everywhere.")
-            print("You knock the door")
-            print("Immediatly, you hear a gun shot and screaming")
-            print("WHO IS THERE?")
+            print("You knock the door..")
+            print("Immediately, you hear a gun shooting and a man screaming:")
+            print("WHO IS THERE?????")
             return 'hunter_house'
 
-        if action == 2:
+        elif action == "2":
             return 'run_forever'
+
+        else:
+            print("\n--------------")
+            print("\nDOES NOT COMPUTE! Type 1 or 2.")
+            return 'main_path'
 
 
 class RunForever(Scene):
 
     def enter(self):
-        print("Forever run down the dark path until the end of the days")
+        print("Forever run down the dark path until the end of the days.")
+        print("--------------")
         exit(1)
 
 
 class HunterHouse(Scene):
 
     def enter(self):
-        print("An old man with a riffle open you the door")
+        print("An old man with a riffle open the door..")
+        print("Two actions are available:")
+        print("""\n 1. Tell a joke
+              \n 2. Stay serious
+              \n """)
+
+        action = input("> ")
+
+        if action in ("1", "Tell a joke"):
+            print("\n--------------")
+            print("Lucky for you they made you learn some Forest insutls.")
+            print("You tell him the joke they tought you at the academy..")
+            print("The stingy hunter laugh at you.")
+            print("You gain all his trust and he shows you the way")
+            print("to the party house, 10 kilometers away after")
+            print("crossing the turbulent river nearby.")
+            return 'river'
+
+        elif action in ("2", "Stay serious"):
+            print("\n--------------")
+            print("The hunter starts thinking that you want")
+            print("to know the best hunting spot.")
+            print("He gets suspicious and he points the gun at you.")
+            return 'shot'
+
+        else:
+            print("\n--------------")
+            print("DOES NOT COMPUTE! Type 1, 2, 'Tell a joke'.")
+            print("or 'Stay serious'.")
+            return 'hunter_house'
 
 
+class River(Scene):
+    def enter(self):
+        print("After a good laugh and a nice walk you reach the river.")
+        print("Close to the river there is a sign:")
+        print("'Unfortunatly the bridge collapsed during last storm.")
+        print("We are working to give you a better bridge this time.")
+        print("Meanwhile KEEP OUT!")
+        print("Do not cross the river until a new bridge is built.")
+        print("The current is strong and only few make it to the other side.'")
 
-        
+        survival_rate = rand.randint(0, 1)
+
+        if survival_rate == 0:
+            print("\n--------------")
+            print("You jump into the river kareless of the warning.")
+            print("Unfortunalty, today was NOT your lucky day.")
+            return 'Drown'
+
+        elif survival_rate == 1:
+            print("\n--------------")
+            print("You cannot read, so you jump into the river anyway.")
+            print("You're damn strong or it is your lucky day..")
+            print("You made it all the way through the other side!")
+            return 'van'
+
+        else:
+            print("C'mon is not hard. Jump into that river.")
+            print("A van is waiting for you on the other side.")
+
+
+class Van(Scene):
+    def enter(self):
+        print("You are now definitely late for this damn dinner.")
+        print("And look at you! You are soaking wet!")
+        print("Your are lucky if they let you in.")
+        print("Anyway there are 10 km left, before your reach the house,")
+        print("and the poor guy in the van has been waiting you until now.")
+
+        drunkness_rate = 0.70
+
+        if rand.random() < drunkness_rate:
+            return 'accident'
+
+        else:
+            print("You come up with huge amount of excuses. The driver")
+            print("does not believe you, but he is nice and he drives you")
+            print("all the way to the party and")
+            print("then he says: 'Enjoy a--hole'.")
+            return 'finished'
+
+
+# Storing each scene by name in a dictionary.
 class Map(object):
     scenes = {
-        'drawn': Drawn(),
+        'Drown': Drown(),
         'shot': Shot(),
         'accident': Accident(),
+        'main_path': MainPath(),
         'hunter_house': HunterHouse(),
-        'run_forever': RunForever()
+        'run_forever': RunForever(),
+        'river': River(),
+        'van': Van()
     }
 
     def __init__(self, start_scene):
         self.start_scene = start_scene
 
     def next_scene(self, scene_name):
-        print("1", Map.scenes.get(scene_name))
         return Map.scenes.get(scene_name)
 
     def opening_scene(self):
-        print("2", self.next_scene(self.start_scene))
         return self.next_scene(self.start_scene)
 
 
