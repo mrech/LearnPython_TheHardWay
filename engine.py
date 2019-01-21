3f932f2867f2ac1e405e04436e73baa5342a2ea3
mrech <rivato.morena@gmail.com>
2019-01-21 19:26:45 +0100

    ex45.py - Lost in the forest

--- engine.py
+++ engine.py
@@ -5,11 +5,8 @@ class Engine(object):
 
     def play(self):
         current_scene = self.scene_map.opening_scene()  # instantiate
-        print("3", current_scene)
 
         while True:
             print("\n--------------")
             next_scene_name = current_scene.enter()
-            print("4", next_scene_name)
             current_scene = self.scene_map.next_scene(next_scene_name)
-            print("5", current_scene)
